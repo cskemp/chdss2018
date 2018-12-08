2491acf671e17413a3711157f2f69a3fafe7c984
charles kemp <ckemp@cmu.edu>
2018-12-08 23:14:19 +1100

    foodweb code now generates correct plots

--- day3_probmodels/foodweb/foodweb_enumerate.R
+++ day3_probmodels/foodweb/foodweb_enumerate.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 
+
+speciesnames <- c("kelp", "herring", "dolphin", "tuna", "sandshark", "mako", "human")
+
 b <- 0.1 # base rate
 t <- 0.5 # transmission rate
 
@@ -23,6 +26,8 @@ cpds <- list( kelp=zerop,
               mako=twop,
               human=onep )
 
+# compute the probability of V, which specifies a value for each species in the foodweb 
+
 p_v <- function(v) {
   prob = cpds$kelp[v$kelp] *
     cpds$herring[v$kelp,v$herring] *
@@ -34,6 +39,77 @@ p_v <- function(v) {
 
   return(prob)
 }
+
+# Compute likelihood assuming weak sampling
+p_obs_given_v <- function(obs, v) {
+  likelihood <- 1
+  for (l in labels(obs)) {
+    if (obs[l] != v[l]) {
+      likelihood <-  0
+    }
+  }
+  return(likelihood)
+}
+
+# create full hypothesis space. Unlike the number game code, here we use 1 and 2 to indicate FALSE and TRUE respectively
+
+n <-  length(speciesnames)
+hs <-  expand.grid(replicate(n, 1:2, simplify = FALSE))
+colnames(hs) <- speciesnames
+hs <- as.tibble(hs)
+nH <- nrow(hs)
+
+# specify observations here
+
+obs <- list(kelp=1, mako=2)
+obs <- list(kelp=1)
+
+hs$prior = NA
+hs$likelihood = NA
+
+for (i in 1:nH) {
+  hs$prior[i] <- p_v(hs[i,])
+  hs$likelihood[i] <- p_obs_given_v(obs, hs[i,])
+}
+
+
+# compute posterior
+
+hs$posterior <- hs$prior* hs$likelihood
+# "normalise" the posterior so that it sums to 1
+hs$posterior <- hs$posterior / sum( hs$posterior ) 
+
+# compute generalization for each animal in foodweb
+
+gen <- hs[1,]
+for( animalname  in speciesnames ) {
+    consistentHypotheses <- as.logical(hs[[animalname]]-1)
+    gen[animalname] <- sum( hs$posterior[consistentHypotheses] ) 
+}
+
+# define plot function
+makeplot <- function(gen) {
+    genplot <- gather(gen)
+    genplot <- genplot %>%
+       mutate(species = factor(key, levels=speciesnames), gen=value) 
+
+    pic <- genplot %>%
+       ggplot(aes(x=species, y=value)) +
+       scale_y_continuous(lim=c(0,1)) +  
+       geom_col() + 
+       theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+       xlab("species") +
+       ylab("prob of having disease") 
+
+    plot(pic)
+}
+
+# call plot function
+makeplot(gen[1:n])
+
+#-------------------------------------------------------------------------------
+# 2. Inference by naive sampling
+
 
 sample_v <- function() {
   tfvals <- c(1,2)
@@ -50,21 +126,31 @@ sample_v <- function() {
 }
 
 
-
-n <-  length(cpds)
-hs <-  expand.grid(replicate(n, 1:2, simplify = FALSE))
-colnames(hs) <- c("kelp", "herring", "dolphin", "tuna", "sandshark", "mako", "human")
-hs <- as.tibble(hs)
-nH <- nrow(hs)
-
-hs$prior = NA
-
-for (i in 1:nH) {
-  hs$prior[i] <- p_v(hs[i,])
+nsample <- 1000
+samples <- data.frame((matrix(NA, nrow = nsample, ncol = n)))
+colnames(samples) <- speciesnames
+samples <- as.tibble(samples)
+for (i in 1:nsample) {
+  samples[i,] <- sample_v()
 }
 
+samples$consistent <- NA
 
-livehs <- hs %>% filter(kelp==1)
-livehs$prior <- livehs$prior / sum(livehs$prior)
+# compute whether each sample is consistent with the observations
+for (i in 1:nsample) {
+  samples$consistent[i] <- p_obs_given_v(obs, samples[i,])
+}
 
-    
+consistentHypotheses <- as.logical(samples$consistent)
+consistentsamples <- samples[consistentHypotheses,1:n]
+gen <- samples[1,]
+for( animalname  in speciesnames ) {
+    # for each species compute the proportion of consistent samples for which it takes value TRUE
+    gen[animalname] <- mean( consistentsamples[[animalname]] ) - 1 
+    # NB: we need to subtract 1 from the mean because we're using 1 and 2 for TRUE and FALSE instead of 0 and 1
+
+}
+
+makeplot(gen[1:n])
+
+
--- day3_probmodels/foodweb/foodweb_jags.R
+++ day3_probmodels/foodweb/foodweb_jags.R
@@ -1,13 +1,16 @@
 library('rjags')
 library('tidybayes')
  
+speciesnames <- c("kelp", "herring", "dolphin", "tuna", "sandshark", "mako", "human")
+
 b <- 0.1 # base rate
 t <- 0.5 # transmission rate
 
 # set up noisy-OR CPDs
 
 zerop <- c(1-b,b)
-onep  <- structure(.Data = c(1-b,b,(1-t)*(1-b),1 - (1-t)*(1-b)), .Dim = c(2,2))
+#onep  <- structure(.Data = c(1-b,b,(1-t)*(1-b),1 - (1-t)*(1-b)), .Dim = c(2,2))
+onep  <- structure(.Data = c(1-b,(1-t)*(1-b),b,1 - (1-t)*(1-b)), .Dim = c(2,2))
 twop  <- structure(.Data = c( 1-b, 
                              (1-t)*(1-b), 
                              (1-t)*(1-b), 
@@ -18,8 +21,8 @@ twop  <- structure(.Data = c( 1-b,
                               1-(1-t)*(1-t)*(1-b)),  .Dim = c(2,2,2))
 
 # specify observations here
-
-obs <- list(kelp=1)
+obs <- list(kelp=1, mako=2)
+obs <- list(mako=2)
 
 foodwebdata<- c(obs, list(
       p.kelp = zerop,
@@ -30,7 +33,6 @@ foodwebdata<- c(obs, list(
       p.mako = twop, 
       p.human = onep))
 
-
 jags <- jags.model('foodweb.bug',data = foodwebdata,
                    n.chains = 4,
                    n.adapt = 100)
@@ -39,26 +41,24 @@ samples <- coda.samples(jags,
              c('kelp', 'herring', 'dolphin', 'tuna', 'sandshark', 'mako', 'human'),
              10000)
 
+# for each species extract the proportion of samples for which it is TRUE
 
-tidysamples <- samples %>% gather_draws(kelp, herring, dolphin, tuna, sandshark, mako, human)
+genplot <- samples %>% 
+        gather_draws(kelp, herring, dolphin, tuna, sandshark, mako, human) %>%
+        rename(species=.variable, gen=.value) %>%
+        ungroup() %>%
+        select(species, gen) %>%
+        mutate(species= factor(species, levels=speciesnames)) %>%
+        group_by(species) %>% 
+        summarize(gen=mean(gen)-1)
 
+pic <- genplot %>% 
+      ggplot(aes(x=species, y=gen)) +
+      scale_y_continuous(lim=c(0,1)) +  
+      geom_col() + 
+      theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+      xlab("species") +
+      ylab("prob of having disease") 
 
-# XXX: make barplot
-
-pic <- tidysamples %>% 
-    ggplot(aes(.value)) +
-    geom_histogram(aes(y=stat(count)), bins=2) +
-    facet_wrap(~.variable) + 
-    xlab("variable value") +
-    ylab("probability")  
-    #scale_y_continuous(lim=c(0,1))   
 plot(pic)
-
-
-# reorder columns for plotting
-# samples <- samples[,c(4,2,1,7,6,5,3)]
-# png('plot_1.png')
-# plot(trace=FALSE,samples, xlim=c(0,2))
-# dev.off()
-
 
